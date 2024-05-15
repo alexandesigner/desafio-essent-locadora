95d6d6f8d849e72a39590b440141dd393df13d8c
Allan Alexandre <alexandesigner@gmail.com>
2024-05-15 16:28:18 -0300

    fix(slides): adjusts size

--- src/components/common/MoviesList.tsx
+++ src/components/common/MoviesList.tsx
@@ -18,14 +18,13 @@ const settings = {
   slidesToShow: 5,
   slidesToScroll: 1,
   centerMode: false,
-  variableWidth: true,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
-        infinite: true,
+        infinite: false,
         dots: true
       }
     },
