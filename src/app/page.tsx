d5cc1f81176a561da46b508a08008b9a095561e9
Allan Alexandre <alexandesigner@gmail.com>
2024-05-15 15:53:43 -0300

    fix(loading): adjusts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Toolbar from '@/components/common/Toolbar';
 import MoviesList from '@/components/common/MoviesList';
@@ -17,18 +17,37 @@ import ListSkeleton from '@/components/common/ListSkeleton';
 import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Home() {
-  const movies = useMovies({ all: true }, { enabled: true });
-  const persons = useMoviePerson({ enabled: true });
-  const moviesData = movies?.data?.data as MovieResponseData[];
-  const personsData = persons?.data?.data?.length
-    ? persons?.data?.data?.slice(0, 6)
-    : (persons?.data?.data as MoviePersonResponseData[]);
-  const moviesGrouped = groupByCategory(moviesData);
+  const { data: moviesResponse } = useMovies({ all: true }, { enabled: true });
+  const { data: personsResponse } = useMoviePerson({ enabled: true });
 
-  const randomMovie =
-    moviesData && moviesData?.length
-      ? moviesData[Math.floor(Math.random() * moviesData?.length)]
-      : null;
+  const [moviesData, setMoviesData] = useState<MovieResponseData[]>([]);
+  const [personsData, setPersonsData] = useState<MoviePersonResponseData[]>([]);
+  const [randomMovie, setRandomMovie] = useState<MovieResponseData | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (moviesResponse?.data) {
+      setMoviesData(moviesResponse.data);
+      setRandomMovie(
+        moviesResponse.data[
+          Math.floor(Math.random() * moviesResponse.data.length)
+        ]
+      );
+    }
+  }, [moviesResponse]);
+
+  useEffect(() => {
+    if (personsResponse?.data) {
+      setPersonsData(
+        personsResponse.data.length > 0
+          ? personsResponse.data.slice(0, 6)
+          : personsResponse.data
+      );
+    }
+  }, [personsResponse]);
+
+  const moviesGrouped = groupByCategory(moviesData);
 
   return (
     <main className='relative'>
@@ -37,7 +56,10 @@ export default function Home() {
         <div className='flex w-full mb-12 relative rounded-bl-[80px] rounded-br-[80px] overflow-hidden'>
           <div className='absolute left-0 bottom-0 w-full h-full bg-gradient-to-t from-black to-transparent'></div>
           <img
-            src={randomMovie?.featured_image ?? 'https://youoatiwlsqmiivcqzoa.supabase.co/storage/v1/object/public/essent-locadora/thumb.png'}
+            src={
+              randomMovie?.featured_image ??
+              'https://youoatiwlsqmiivcqzoa.supabase.co/storage/v1/object/public/essent-locadora/thumb.png'
+            }
             alt='Banner de destaque'
             className='w-full h-[680px] object-cover rounded-bl-[80px] rounded-br-[80px]'
             onError={(e) => {
@@ -54,7 +76,9 @@ export default function Home() {
             </h3>
             <p className='text-white text-lg mt-4'>{randomMovie?.title}</p>
             <Link
-              href={`/${slugify(randomMovie?.category?.name)}/${randomMovie?.id}_${slugify(randomMovie?.title)}`}
+              href={`/${slugify(randomMovie?.category?.name)}/${
+                randomMovie?.id
+              }_${slugify(randomMovie?.title)}`}
               title='Ver detalhes'
               className='bg-white font-bold max-w-[220px] text-center text-primary mt-6 rounded-lg py-4 px-12 text-lg'
             >
@@ -71,9 +95,9 @@ export default function Home() {
       <div className='py-8 px-4 w-full max-w-[1520px] mx-auto'>
         <MoviePersonList
           data={personsData}
-          isLoading={!persons?.data?.meta?.ok}
+          isLoading={!personsResponse?.meta?.ok}
         />
-        {movies?.isLoading ? (
+        {!personsResponse?.meta?.ok ? (
           <div className='mx-4 w-full py-8 flex flex-col gap-4'>
             <Skeleton className='w-[220px] h-8 mb-8 mx-4' />
             <ListSkeleton />
@@ -88,7 +112,7 @@ export default function Home() {
                 title={item.title}
                 data={item.data}
                 slugPath={item.slugPath}
-                isLoading={!movies?.data?.meta?.ok}
+                isLoading={!moviesResponse?.meta?.ok}
               />
             </React.Fragment>
           ))
